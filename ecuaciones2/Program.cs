86a2b6815ce7aa320fa5b2047a30309201690dfb
datasoftuy <andres.esteves.aharonian@gmail.com>
2017-05-10 16:32:51 -0300

    codigo ordenado

--- ecuaciones2/Program.cs
+++ ecuaciones2/Program.cs
@@ -27,16 +27,10 @@ namespace leapyear
                 Console.WriteLine("Sistemas de ecuaciones de segundo grado");
 
                 Console.Write("Ingrese a ");
-                
                 a = Convert.ToDouble(Console.ReadLine());
 
-
-
                 Console.Write("Ingrese b ");
-
-
                 b = Convert.ToDouble(Console.ReadLine());
-
 
                 Console.Write("Ingrese c ");
                 c = Convert.ToDouble(Console.ReadLine());
@@ -45,18 +39,12 @@ namespace leapyear
                 x1 = ((-b + Math.Sqrt(Math.Pow(b, 2) - (4 * a * c))) / (2 * a));
                 x2 = ((-b - Math.Sqrt(Math.Pow(b, 2) - (4 * a * c))) / (2 * a));
 
-               
+
 
 
                 // Codigo solo para verificar valor de x1 y x2
                 Console.WriteLine("x1 " + x1);
                 Console.WriteLine("x2 " + x2);
-
-
-
-
-
-
 
                 Console.WriteLine("Desea calcular otra ecuacion s/n");
                 otravez = Console.ReadLine().ToLower();
@@ -77,3 +65,14 @@ namespace leapyear
         }
     }
 }
+
+                
+
+
+
+
+
+
+
+
+
